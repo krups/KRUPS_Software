a17792823e1cf00e51b3d686e92135d7c670ddba
Collin Dietz <c4dietz@gmail.com>
2017-05-10 10:44:45 -0400

    Added a queue, moved sensor readings

--- KRUPS_Software/KRUPS_Software.ino
+++ KRUPS_Software/KRUPS_Software.ino
@@ -1,16 +1,56 @@
 #include <Arduino.h>
-//#include <SPI.h>
-
 #include <KRUPS_TC.h>
 #include <KRUPS_Sensor.h>
-#include <compress.h>
-//#include <Snooze.h>
+#include<compress.h>
 #include <IridiumSBD.h>
+#include <QueueList.h>
 
-volatile bool launched = false, ejected = false, splash_down = false;
-uint8_t measure_buf[32]; // don't forget to change to a larger size
+
+
+#define BUFF_SIZE   (430) //size of the measurement buffer
+#define PACKET_SIZE (44) //size of a packets to send
+#define HEADER_SIZE (2)  //number of bytes taken up in the packet by buffer
+#define TC_NUM (12) //number of TC for testing 
+
+volatile bool launched = false, ejected = true, splash_down = false;
+uint8_t measure_buf[BUFF_SIZE]; // don't forget to change to a larger size
 size_t loc = 0;
 IridiumSBD isbd(Serial1);
+int16_t num_packets = 0; //counter for number of packets to establish chronlogical order
+QueueList<uint8_t*> message_queue; //queue of messages left to send, messages pushed into front, pulled out back
+
+//Fake read funcitons for testing 
+/*
+void Read_gyro(uint8_t *buf, size_t &loc)
+{
+    append(buf, loc, 0+num_packets);
+    append(buf, loc, 1+num_packets);
+    append(buf, loc, 2+num_packets);
+}
+
+void Read_loaccel(uint8_t *buf, size_t &loc)
+{
+    append(buf, loc, 10+num_packets);
+    append(buf, loc, 11+num_packets);
+    append(buf, loc, 12+num_packets);
+}
+
+void Read_mag(uint8_t *buf, size_t &loc)
+{
+    append(buf, loc, 20+num_packets);
+    append(buf, loc, 21+num_packets);
+    append(buf, loc, 22+num_packets);
+}
+
+void Read_TC(uint8_t *buf, size_t &loc)
+{
+  for(int i = 0; i < TC_NUM; i++)
+  {
+    append(buf, loc, i*7 + num_packets);
+  }
+  delay(TC_NUM/3 * 100);
+}
+*/
 
 void setup() {
     init_Sensors(); init_TC();
@@ -19,7 +59,44 @@ void setup() {
     isbd.begin();
     isbd.adjustSendReceiveTimeout(45);
     isbd.useMSSTMWorkaround(false);
-    isbd.setPowerProfile(0);
+}
+
+
+void do_tasks()
+{
+        Read_gyro(measure_buf, loc);        // 6 bytes
+        Read_loaccel(measure_buf, loc);     // 6 bytes
+        //Read_hiaccel(measure_buf, loc);     // 6 bytes
+        Read_mag(measure_buf, loc);         // 6 bytes
+        Read_TC(measure_buf, loc);          // 16 bytes
+        // TODO: store the measurements in the EEPROM/FLASH
+        // TODO: reorder packets for transmission
+        
+        //NOTE: CHANGE TO LESS STRINGENT CONDITION make it a multiple of total read size
+        if(loc >= PACKET_SIZE - HEADER_SIZE)  //if we have enough bytes to fill the packet (leaving space for the header)
+        {
+            //TODO: make any edits to packet header etc
+            //use counter to number packet
+            uint8_t packet[PACKET_SIZE]; 
+            size_t p_loc = 0;
+
+            //add header
+            append(packet, p_loc, num_packets); // add counter to the front of the packet
+
+            //copy readings
+            for(int i = 0; i < PACKET_SIZE-HEADER_SIZE; i++)
+            {
+                packet[p_loc] = measure_buf[i];
+                p_loc++;
+            }
+
+            message_queue.push(packet);
+            Serial.println("Packet added");
+            num_packets++; //we now have one more packet
+
+            //reset where the sensors are writing to in the buffer
+            loc = 0;
+        }
 }
 
 void loop() {
@@ -28,17 +105,16 @@ void loop() {
         // potentially start GPS ping and LED beacon
     }
     else if (ejected) {
-        Read_gyro(measure_buf, loc);        // 6 bytes
-        Read_loaccel(measure_buf, loc);     // 6 bytes
-        Read_hiaccel(measure_buf, loc);     // 6 bytes
-        Read_mag(measure_buf, loc);         // 6 bytes
-        Read_TC(measure_buf, loc);          // 16 bytes
-
-        // TODO: store the measurements in the EEPROM/FLASH
-        // TODO: reorder packets for transmission
-
+        do_tasks(); //complete needed tasks for measurment
         // TODO: transmit data packets
-        isbd.sendSBDBinary(measure_buf, loc);
+        if(!message_queue.isEmpty())
+        {
+            uint8_t* packet;
+            packet = message_queue.peek();
+            uint16_t loc;
+            isbd.sendSBDBinary(packet, loc);
+            message_queue.pop();
+        }
     }
     else if (launched) {
         // wait for ejection
@@ -47,16 +123,11 @@ void loop() {
 
 void launch(void) {
     launched = true;
-    read8(L3GD20_ADDRESS, GYRO_REGISTER_INT1_SRC);
+    //read8(L3GD20_ADDRESS, GYRO_REGISTER_INT1_SRC);
 }
 
+///run if packet is not sent on a try
 bool ISBDCallback() {
-    Read_gyro(measure_buf, loc);
-    Read_loaccel(measure_buf, loc);
-    Read_hiaccel(measure_buf, loc);
-    Read_mag(measure_buf, loc);
-    Read_TC(measure_buf, loc);
-
-
+    do_tasks(); //make sure we are still reading in measurements and building packets
     return true;
 }
