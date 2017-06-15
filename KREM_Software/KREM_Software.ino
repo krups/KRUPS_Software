ce23e46f930a273e7feaf3366dccc932acff88f6
Evan Whitmer <evancw@gmail.com>
2017-06-15 14:57:32 -0400

    Changes to KREM

--- KREM_Software/KREM_Software.ino
+++ KREM_Software/KREM_Software.ino
@@ -1,24 +1,30 @@
 // Solenoid control requires 500ms to fully extend
 #define cam1    (11)
 #define cam2    (12)
-#define door    (14)
-#define eject   (15)
+#define door    (15)
+#define eject   (14)
 #define timer   (13)
 #define sensor  (10)
 
 void setup() {
+    Serial.begin(9600);
+    while(!Serial);
   // put your setup code here, to run once:
     pinMode(cam1, OUTPUT);
     pinMode(cam2, OUTPUT);
     pinMode(door, OUTPUT);
     pinMode(eject, OUTPUT);
     pinMode(sensor, OUTPUT);
+    pinMode(timer, INPUT);
 
     // wait until timer event triggers
+    Serial.println("untriggered");
     while(digitalRead(timer) != HIGH);
+    Serial.println("triggered");
 
     // delay for safety
-    delay(5700);
+    delay(700);
+    Serial.println("done delaying");
  
     // cameras on
     digitalWrite(cam1, HIGH);
@@ -26,18 +32,23 @@ void setup() {
     delay(100);
     digitalWrite(cam1, LOW);
     digitalWrite(cam2, LOW);
+    Serial.println("cams on");
     
     // sensor board on
-    digitalWrite(sensor, HIGH)
+    digitalWrite(sensor, HIGH);
     delay(100);
     digitalWrite(sensor, LOW);
-    
+    Serial.println("sensor on");
+
+    delay(3500); //
     // door open
     digitalWrite(door, HIGH);
     delay(500);
     digitalWrite(door, LOW);
+    Serial.println("door open");
     
     // sensor board on (redundant)
+    delay(2000);
     digitalWrite(sensor, HIGH);
     delay(100);
     digitalWrite(sensor, LOW);
@@ -46,6 +57,7 @@ void setup() {
     digitalWrite(eject, HIGH);
     delay(500);
     digitalWrite(eject, LOW);
+    Serial.println("ejected");
 }
 
 void loop() {
