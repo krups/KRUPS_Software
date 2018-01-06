bf87f37b164bcf2552866952a0233267b932f42f
Collin Dietz <c4dietz@gmail.com>
2018-01-06 13:14:31 -0500

    Added KICC Software for KICC Serial communication

---
+++ KICC_Software/KICC_Software.ino
@@ -0,0 +1,25 @@
+#define CAPSULE_SERIAL (Serial2)
+#define KICC_SERIAL (Serial)
+#define BAUD (9600)
+void setup()
+{
+  CAPSULE_SERIAL.begin(BAUD);
+  KICC_SERIAL.begin(BAUD);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+}
+
+void loop()
+{
+  if(CAPSULE_SERIAL.available())
+  {
+    KICC_SERIAL.write(CAPSULE_SERIAL.read());
+  }
+
+  if(KICC_SERIAL.available())
+  {
+    CAPSULE_SERIAL.write(KICC_SERIAL.read());
+  }
+  
+}
+
